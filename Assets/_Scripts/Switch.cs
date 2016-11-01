4f23e72210dec390b025935c1596b51011649ffd
Cadoni1 <xbartonc@gmail.com>
2016-11-01 13:41:54 -0600

    Added switch code.

---
+++ Assets/_Scripts/Switch.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour {
+
+	private Animator animator;
+
+	public GameObject[] switches;
+
+	// Use this for initialization
+	void Start () 
+	{
+		animator = GetComponent<Animator> ();
+		switches = GameObject.FindGameObjectsWithTag("Switch");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "Projectile")
+		{
+			foreach (GameObject el in switches) 
+			{
+				el.GetComponent<Animator> ().SetTrigger ("isHit");
+			}
+		}
+	}
+}
