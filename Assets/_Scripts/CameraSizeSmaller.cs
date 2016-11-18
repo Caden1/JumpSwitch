ff71c054c07cedd24cf10fe5e10baf5271d4b492
Caden <xbartonc@gmail.com>
2016-11-18 16:29:06 -0700

    Made the camera size bigger at certain spots. Added a song on level 1.

---
+++ Assets/_Scripts/CameraSizeSmaller.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraSizeSmaller : MonoBehaviour
+{
+    public Camera mainCamera; // Reference to the main camera
+
+    private float cameraOriginalSize;
+
+    // Use this for initialization
+    void Start ()
+    {
+        cameraOriginalSize = mainCamera.orthographicSize; // Gets the starting size of the main camera
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.tag == "Player") // When the collider is collided with
+        {
+            mainCamera.orthographicSize = cameraOriginalSize;
+        }
+    }
+}
